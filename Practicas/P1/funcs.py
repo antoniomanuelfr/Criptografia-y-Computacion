575875e27306a58da5e34bd50335f3ab77274158
Antonio Manuel Fresneda Rodriguez <antoniomanuelfr@gmail.com>
2019-03-11 16:48:29 +0100

    p1 apartados 1 a 5

--- Practicas/P1/funcs.py
+++ Practicas/P1/funcs.py
@@ -34,47 +34,62 @@ def Descomponer (a):
         b = b //  2
     return cont, b
 
-def Miller_Rabin (p):
+def Miller_Rabin (p, pr=False):
 
     if p >= 5 and p %2 ==1:
         u,s = Descomponer (p-1)
         a = randint(2, p-2)
-        print ("Estamos usando como testigo: a = ", a)
+        if pr:
+            print ("Estamos usando como testigo: a = ", a)
         res = potencia_modular(a,s,p)
-
-        print (a, "^", s, " = ", res, "mod ", p)
+        if pr:
+            print ("{} ^ {} = {} mod {}".format(res,s,res,p))
 
         if res == 1 or res == p-1: 
             return True
 
         for i in range (1,u):
-            a *= a
-            res = potencia_modular(a,s,p)
-            print (a, "^", s*(2**i), " = ", res, "mod ", p)
-
+            res = potencia_modular(res,2,p)
+            if pr:
+                print ("{} ^ {} = {} mod {}".format(res,s*(2**i),res,p))
             if res == p-1:
                 return True
             elif res ==1:
                 return False
     return False
 
+def primo(p, n=10, pr=False):
+
+    for i in range (n):
+        if not Miller_Rabin(p):
+            if pr:
+                print (p, " no es primo\n")
+            return False
+    if pr :
+        print (p, " es posible primo\n")
+    return True
+
+def siguiente_primo(p): 
+
+    aux = p+1 if p % 2 == 0 else p+2
+    
+    while not Miller_Rabin(aux): 
+        aux += 2
+    return aux
+    
+def primo_n_bits(n):
+    sem = randint(2**n-1, 2**n)
+    return siguiente_primo(sem)
+
+def primo_fuerte(n):
+
+    pr = siguiente_primo(n)
+    while not primo( (pr-1) // 2): 
+        pr = siguiente_primo(pr)
+    return pr
+
 if __name__ == '__main__':
 
-    # p= 879976242195951958890801816612768566943805170226410617823301865416003514546684111640331356490455690766475339038983303063831818394885482954417406863802340357540397021808027209610884076158915519334125353771492979
-    # p+=2
-    # p=2199733160881
-    # print (potencia_modular(21997331,p-1, p))
-    # print (potencia_modular(2193323,p-1, p))
-    # print (potencia_modular(4434342,p-1, p))
-    # print (potencia_modular(2,p-1, p))
-    # print (potencia_modular(432423444,p-1, p))
-    # print (potencia_modular(20,35, 561))
-
-    # #si m es primo x^2 -1 =0 tiene dos soluciones en zn
-    # # si m es impar y producto de n primos x^2 -1 = 0 tiene 2^n soluciones 
-    for i in range (5):
-        if Miller_Rabin(561):
-            print ("Posible primo\n")
-        else:
-            print ("No es primo\n")
-
+    print (primo_n_bits(2))
+    print (siguiente_primo(5))
+    print(primo_fuerte(23))
